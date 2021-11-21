d8ed80981126c093b1cfc68429bd94c150e0a4df
pemeoh <94102125+pemeoh@users.noreply.github.com>
2021-11-22 00:42:19 +0700

    Add files via upload

---
+++ DvAnTV.js
@@ -0,0 +1,25 @@
+class DvAnTV extends Animal{
+    #_food;
+    constructor(name,gender,food){
+        super(name,gender);                        
+        this.Food = food;
+    }
+    set Food(food){
+        if(food === FOOD.FRUIT || FOOD.GRASS){
+            this.#_food = food;
+        }
+        else{
+            alert("Wrong food !");
+            throw new Error("Wrong food !");
+        }
+    }
+    get Food(){
+        return this.#_food;
+    }
+    Eat(){
+        document.writeln(this.Name + " is eating " + this.Food + "<br>");
+    }
+    Move(){
+        document.writeln(this.Name + " is moving !<br>");
+    }
+}
