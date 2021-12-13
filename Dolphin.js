7003270590e3648b3ac96dcafd78613b3f5775bd
Đào Ngọc Thạch <94102125+Daongocthach@users.noreply.github.com>
2021-12-13 13:40:27 +0700

    Update Dolphin.js

--- Dolphin.js
+++ Dolphin.js
@@ -1,92 +1,117 @@
-class Dolphin extends DvAnThit{
+class Dolphin extends DvAnThit
+{
     #_energy = Normal_Status.ENERGY;
     #_hungry = Normal_Status.HUNGRY;
     #_mood = Normal_Status.MOOD;
     #_Swimming_speed = Math.floor(Math.random() * 11);
-    constructor(name,gender,food,age){
+    constructor(name,gender,food,age)
+    {
         super(name,gender,food,age);
         Dolphin.#_number_of_dolphins++;
     }
     static #_number_of_dolphins = 0;
-    static Compare_swimming_speed(d1,d2){
-        if(d1.Swimming_speed < d2.Swimming_speed){
+    static Compare_swimming_speed(d1,d2)
+    {
+        if(d1.Swimming_speed < d2.Swimming_speed)
+        {
             document.writeln(d2.Name + " has better swimming skill than " + d1.Name + " !<br>");
         }
-        else if(d1.Swimming_speed > d2.Swimming_speed){
+        else if(d1.Swimming_speed > d2.Swimming_speed)
+        {
             document.writeln(d1.Name + " has better swimming skill than " + d2.Name + " !<br>");
         }
         else document.writeln(d1.Name + " and " + d2.Name + " are equal !<br>");
     }
-    get Number_of_dolphins(){
+    get Number_of_dolphins()
+    {
         return Dolphin.#_number_of_dolphins;
     }
-    get Swimming_speed(){
+    get Swimming_speed()
+    {
         return this.#_Swimming_speed;
     }
-    Play(){
-        if(this.#_energy > 0){
+    Play()
+    {
+        if(this.#_energy > 0)
+        {
             document.writeln(this.Name + " is playing !<br>");
             this.Make_Sound();
             this.#_mood++;
             this.#_energy--;
             this.#_hungry++;
         }
-        else{
+        else
+        {
             alert(this.Name + " is not enough energy !");
         }
     }
-    Move(){
-        this.#Swim();
-        if(this.#_energy > 0 ){
+    Move()
+    {
+        if(this.#_energy > 0 )
+        {
+            this.#Swim();
             this.#_energy--;
             this.#_hungry++;
         }
         else alert(this.Name + " is not enough energy !");
     }
-    #Swim(){
+    #Swim()
+    {
         document.writeln(this.Name + " is swimming !<br>");
     }
-    Eat(){
+    Eat()
+    {
         super.Eat();
         this.#_hungry = 0;
         this.Make_Sound();
     }
-    Make_Sound(){
+    Make_Sound()
+    {
         document.writeln("Ririri !<br>");
     }
-    Sleep(){
-        if(this.#_energy < 10){
+    Sleep()
+    {
+        if(this.#_energy < 10)
+        {
             document.writeln(this.Name + " is sleeping ZZzzzz~~<br>");
             this.#_energy++;
             this.#_hungry++;
         }
         else alert(this.Name + " is full energy !");
     }
-    Check_Status(){
+    Check_Status()
+    {
         var flag = true;
-        if(this.#_hungry > 8){
+        if(this.#_hungry > 8)
+        {
             this.#_mood = 0;
             document.writeln(this.Name + " is hungry !<br>");
             flag = false;
         }
-        else{
+        else
+        {
             document.writeln(this.Name + " is not hungry !<br>");
         }
-        if(this.#_mood === 0 ){
+        if(this.#_mood === 0 )
+        {
             document.writeln(this.Name + " is not happy !<br>");
             flag = false;
         }
-        else{
+        else
+        {
             document.writeln(this.Name + " is happy !<br>");
         }
-        if(this.#_energy <= 2){
+        if(this.#_energy <= 2)
+        {
             document.writeln(this.Name + " is tired !<br>");
             flag = false;
         }
-        else{
+        else
+        {
             document.writeln(this.Name + " is excited !<br>");
         }
-        if(flag === true){
+        if(flag === true)
+        {
             document.writeln(this.Name + " is healthy !<br>");
         }
         else document.writeln(this.Name + " is not healthy !<br>")
