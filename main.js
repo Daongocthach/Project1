e79e16cf06d2d2ba9805f528f00014d1ac4036f1
Trandinhdongkhanh <95671871+Trandinhdongkhanh@users.noreply.github.com>
2021-12-13 17:12:50 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,4 @@ let cow1 = new Cow("Suzy",GENDER.FEMALE,FOOD.GRASS,15);
 cow1.Check_Status();
 let cow2 = new Cow("Tom",GENDER.MALE,FOOD.GRASS,13);
 Cow.Compare_Size(cow1,cow2); 
+document.writeln(Cow.Number_of_cows);
