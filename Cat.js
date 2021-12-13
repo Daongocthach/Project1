eb79eab0ed03f4353b982eaaeb4abb7cd985780b
Trandinhdongkhanh <95671871+Trandinhdongkhanh@users.noreply.github.com>
2021-12-13 17:14:27 +0700

    Add files via upload

--- Cat.js
+++ Cat.js
@@ -1,122 +1,101 @@
-class Cat extends DvAnThit
-{
+class Cat extends DvAnThit{
     #_energy = Normal_Status.ENERGY;            //Moi con vat khi dc khoi tao deu se co nhung chi so mac dinh
     #_hungry = Normal_Status.HUNGRY;            
     #_mood = Normal_Status.MOOD;
     #_night_vision_point = Math.floor(Math.random() * 11);  //Random cac so tu 0 den 10
-    constructor(name,gender,food,age)    //Math.random() tra ve 1 so ngau nhien co gtri tu [0,1)
-    {                                    //Math.random() ket hop vs Math.floor() co the dc su dung de tra ve so nguyen
-        super(name,gender,food,age);                           
+    constructor(name,gender,food,age){                          //Math.random() tra ve 1 so ngau nhien co gtri tu [0,1)
+        super(name,gender,food,age);                            //Math.random() ket hop vs Math.floor() co the dc su dung de tra ve so nguyen
         Cat.#_number_of_cats++;
         document.writeln("<br>")
     }
-    get Night_vision_point()             //Moi con meo khi dc tao se co diem tam nhin rieng ko thay doi dc nen chi co Get
-    {            
+    get Night_vision_point(){             //Moi con meo khi dc tao se co diem tam nhin rieng ko thay doi dc nen chi co Get
         return this.#_night_vision_point;
     }
     static #_number_of_cats = 0;            //private static field
-    static get Number_of_cats()               //static property
-    {          
+    static get Number_of_cats(){            //static property
         return Cat.#_number_of_cats;
     }
-    static Catch_Mouse(cat)
-    {
+    static Catch_Mouse(cat){
         document.writeln(cat.Name + " is catching a mouse ! <br>");      //tu khoa <br> de xuong dong tren trang html
     }
-    static Compare_night_vision(cat1,cat2)   //Static method
-    {        
-        if(cat1.Night_vision_point < cat2.Night_vision_point)
-        {
+    static Compare_night_vision(cat1,cat2){         //Static method
+        if(cat1.Night_vision_point < cat2.Night_vision_point){
             document.writeln(cat2.Name + " has better night vision than " + cat1.Name + " !<br>")
         }
-        else if(cat1.Night_vision_point > cat2.Night_vision_point)
-        {
+        else if(cat1.Night_vision_point > cat2.Night_vision_point){
             document.writeln(cat1.Name + " has better night vision than " + cat2.Name + " !<br>")
         }
         else document.writeln(cat1.Name + " and " + cat2.Name + " are equal !<br>")
     }
-    Meow()
-    {
+    #Meow(){
         document.writeln("Meow !<br>");
     }
-    Play()
-    {
-        if(this.#_energy > 0)
-        {
+    Make_Sound(){
+        super.Make_Sound();
+        this.#Meow();
+    }
+    Play(){
+        if(this.#_energy > 0){
             document.writeln(this.Name + " is playing !<br>");
             this.Meow();
             this.#_mood++;                    
             this.#_energy--;
             this.#_hungry++;
         }
-        else
-        {
+        else{
             alert(this.Name + " is not enough energy !");
         }
     }
     //override lai phuong thuc Move()
-    Move()
-    {
-        if(this.#_energy > 0 )
-        {
-            this.#Run();
+    Move(){
+        this.#Run();
+        if(this.#_energy > 0 ){
             this.#_energy--;
             this.#_hungry++;
         }
         else alert(this.Name + " is not enough energy !");
     }
-    #Run()
-    {
+    #Run(){
         document.writeln(this.Name + " is running !<br>");
     }
-    Eat()
-    {
+    Eat(){
         super.Eat();
         this.#_hungry = 0;
         this.Meow();
     }
-    Sleep()
-    {
-        if(this.#_energy < 10)
-        {
+    Sleep(){
+        if(this.#_energy < 10){
             document.writeln(this.Name + " is sleeping ZZzzzz~~<br>");
             this.#_energy++;
             this.#_hungry++;
         }
         else alert(this.Name + " is full energy !");
     }
-    Check_Status()
-    {
+    Check_Status(){
         var flag = true;
-        if(this.#_hungry > 8)
-        {
+        if(this.#_hungry > 8){
             this.#_mood = 0;
             document.writeln(this.Name + " is hungry !<br>");
             flag = false;
         }
-        else
-        {
+        else{
             document.writeln(this.Name + " is not hungry !<br>");
         }
-        if(this.#_mood === 0 )
-        {
+        if(this.#_mood === 0 ){
             document.writeln(this.Name + " is not happy !<br>");
             flag = false;
         }
-        else
-        {
+        else{
             document.writeln(this.Name + " is happy !<br>");
         }
         if(this.#_energy <= 2){
             document.writeln(this.Name + " is tired !<br>");
             flag = false;
         }
-        else
-        {
+        else{
             document.writeln(this.Name + " is excited !<br>");
         }
-        if(flag === true)
-        {
+        if(flag === true){
             document.writeln(this.Name + " is healthy !<br>");
         }
         else document.writeln(this.Name + " is not healthy !<br>")
--- DvAnThit.js
+++ DvAnThit.js
@@ -1,33 +1,28 @@
-class DvAnThit extends Animal
-{
+class DvAnThit extends Animal{
     #_food;
-    constructor(name,gender,food,age)
-    {
+    constructor(name,gender,food,age){
         super(name,gender,age);                        
         this.Food = food;
     }
-    set Food(food)
-    {
-        if(food === FOOD.BEEF || food === FOOD.FISH || food === FOOD.PORK )
-        {
+    set Food(food){
+        if(food === FOOD.BEEF || food === FOOD.FISH || food === FOOD.PORK ){
             this.#_food = food;
         }
-        else
-        {
+        else{
             alert("Wrong food !");
             throw new Error("Wrong food !");            //Thong bao loi tren console va dung chuong trinh
         }
     }
-    get Food()
-    {
+    get Food(){
         return this.#_food;
     }
-    Eat()       //override abstract method
-    {
+    Eat(){
         document.writeln(this.Name + " is eating " + this.Food +"<br>");
     }
-    Move()
-    {
+    Move(){
         document.writeln(this.Name + " is moving !<br>");
+    }
+    Make_Sound(){
+        document.writeln(this.Name + " is making a sound !<br>");
     }
 }
