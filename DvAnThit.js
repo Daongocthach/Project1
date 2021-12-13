210879e699fbf661ccff004d8222714a37eada32
Đào Ngọc Thạch <94102125+Daongocthach@users.noreply.github.com>
2021-12-13 13:32:52 +0700

    Update DvAnThit.js

--- DvAnThit.js
+++ DvAnThit.js
@@ -1,25 +1,33 @@
-class DvAnThit extends Animal{
+class DvAnThit extends Animal
+{
     #_food;
-    constructor(name,gender,food,age){
+    constructor(name,gender,food,age)
+    {
         super(name,gender,age);                        
         this.Food = food;
     }
-    set Food(food){
-        if(food === FOOD.BEEF || food === FOOD.FISH || food === FOOD.PORK ){
+    set Food(food)
+    {
+        if(food === FOOD.BEEF || food === FOOD.FISH || food === FOOD.PORK )
+        {
             this.#_food = food;
         }
-        else{
+        else
+        {
             alert("Wrong food !");
             throw new Error("Wrong food !");            //Thong bao loi tren console va dung chuong trinh
         }
     }
-    get Food(){
+    get Food()
+    {
         return this.#_food;
     }
-    Eat(){
+    Eat()       //override abstract method
+    {
         document.writeln(this.Name + " is eating " + this.Food +"<br>");
     }
-    Move(){
+    Move()
+    {
         document.writeln(this.Name + " is moving !<br>");
     }
 }
